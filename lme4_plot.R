580e13e15a4ee97873f12b6c736fd0633627c45d
AparicioJohan <44405276+AparicioJohan@users.noreply.github.com>
2019-06-14 10:06:57 -0500

    Add files via upload

--- lme4_plot.R
+++ lme4_plot.R
@@ -7,25 +7,25 @@ lme4.plot <-
     y.coord <- Datos[,row]
     response <- Datos[, names(x@frame)[1]]  # !is.na(Datos[,names(x@frame)[1]]) 
     Datos$rowname <- rownames(Datos)
+    
     residuals <- data.frame(rowname=names(residuals(x)), residuals= as.numeric(residuals(x)))
     residuals <- merge(residuals,Datos,by="rowname", all = T)
     residuals <- arrange(residuals,col,row)$residuals
+    
     fitted <- data.frame(rowname=names(fitted.values(x)), fitted=as.numeric(fitted.values(x)))
     fitted <- merge(fitted,Datos,by="rowname", all = T)
     fitted <- arrange(fitted,col,row)$fitted
+    
     geno.pred <- data.frame(genotype=rownames(ranef(x)[[gen]])   , predicted.value=ranef(x)[[gen]][,1])
     names(geno.pred)[1] <- gen
-
     columns <- seq(min(x.coord), max(x.coord), by = min(diff(sort(unique(x.coord)))))
     rows <- seq(min(y.coord), max(y.coord), by = min(diff(sort(unique(y.coord)))))
-    
     xy.coord <- data.table(expand.grid(columns = columns, rows = rows))
-    
     v <- merge(geno.pred,Datos,by = gen,all.y = TRUE)
-    
     xx <- arrange(v,col,row)
     geno.pred <- as.vector(xx$predicted.value)
     
+    environment <- fitted-geno.pred-fixef(x)["(Intercept)"]
     
     setNumericRounding(2)
     
@@ -34,7 +34,7 @@ lme4.plot <-
     setkeyv(xy.coord, c("rows", "columns"))
     ONE <- rep(1, length(x.coord))    
     df <- data.table(columns = x.coord, rows = y.coord, 
-                     response = response, fitted = fitted,
+                     response = response, fitted = fitted,environment,
                      residuals = residuals, geno.pred = geno.pred, ONE = ONE)
     setkeyv(df, c("rows", "columns"))
     df <- df[xy.coord]
@@ -42,7 +42,7 @@ lme4.plot <-
     
     colors = topo.colors(100)
     
-    main.legends <- c('Raw data', 'Fitted data',  "Ind Residuals" , 'Residuals', "Genotypic BLUPs", 'Histogram')
+    main.legends <- c('Raw data', 'Fitted data', 'Residuals',"Effect Design"  ,"Genotypic BLUPs", 'Histogram')
     if(all.in.one) {
       op <- par(mfrow = c(2,3), oma = c(ifelse(annotated, 12, 2), 1, 3, 2), mar = c(2.7, 4, 2.5, 2.5), mgp = c(1.7, 0.5, 0))                
     } else {
@@ -57,7 +57,10 @@ lme4.plot <-
     fields::image.plot(columns, rows, t(matrix(df$fitted, ncol = length(columns), nrow = length(rows))), main = main.legends[2], col = colors, xlab = xlab, ylab = ylab, zlim = range, graphics.reset = TRUE, ...)
     if(!all.in.one)
       readline("Press return for next page....")
-    fields::image.plot(columns, rows, t(matrix(df$residuals, ncol = length(columns), nrow = length(rows))), main = main.legends[4], col = colors, xlab = xlab, ylab = ylab, graphics.reset = TRUE, ...)
+    fields::image.plot(columns, rows, t(matrix(df$residuals, ncol = length(columns), nrow = length(rows))), main = main.legends[3], col = colors, xlab = xlab, ylab = ylab, graphics.reset = TRUE, ...)
+    if(!all.in.one)
+      readline("Press return for next page....")
+    fields::image.plot(columns, rows, t(matrix(df$environment, ncol = length(columns), nrow = length(rows))), main = main.legends[4], col = colors, xlab = xlab, ylab = ylab, graphics.reset = TRUE, ...)
     if(!all.in.one)
       readline("Press return for next page....")
     fields::image.plot(columns, rows, t(matrix(df$geno.pred, ncol = length(columns), nrow = length(rows))), main = main.legends[5], col = colors, xlab = xlab, ylab = ylab, graphics.reset = TRUE, ...)
