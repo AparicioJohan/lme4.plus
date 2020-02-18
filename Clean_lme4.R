710a226b6627dbd6e0de004d27e3eccd10025926
AparicioJohan <44405276+AparicioJohan@users.noreply.github.com>
2020-02-18 14:44:19 -0500

    Update Clean_lme4.R

--- Clean_lme4.R
+++ Clean_lme4.R
@@ -1,4 +1,5 @@
 
+
 # Function for clean_datasets using lme4
 
 library(lmerTest)
@@ -16,19 +17,19 @@ Clean_lme4 <- function(Response, Geno , Num_desv=3, Show_results=TRUE, data=NULL
 
   remFinal <- data.frame(Response=as.numeric(),  Genotype = as.character(), rep=as.numeric(),block=as.numeric() )
 
-  m_alpha <- paste(Response, "~" , "1 + (1|" , Genotype, ") + " , rep , "+ (1|", rep,":", block, ")")
-  m_rcbd <- paste(Response, "~" , "1 + (1|" , Genotype, ") + " , rep )
+  m_alpha <- paste(Response, "~" , "1 + (1|" , Geno, ") + " , rep , "+ (1|", rep,":", block, ")")
+  m_rcbd <- paste(Response, "~" , "1 + (1|" , Geno, ") + " , rep )
   Model <-  c(m_alpha, m_rcbd)
   cond=  length(unique(Datos$block))
   mvOn = F
   ix = c()
   
-  reps <- sum(is.na(Datos[,"Response"] ))
+  reps <- sum(is.na(Datos[,Response] ))
   
   if(cond == nrow(Datos)) return()
   if(reps >= 0.6*nrow(Datos)) return()
   if(length(unique(Datos[,rep]))==1) return()
-  if( sum(is.na(Datos[,Genotype]))>0.5*length(Datos[,Genotype]) )  return()
+  if( sum(is.na(Datos[,Geno]))>0.5*length(Datos[,Geno]) )  return()
   if( sum(is.na(Datos[,Response]))>0.5*length(Datos[,Response])) return()
   
   if (cond>1) {
@@ -48,13 +49,13 @@ Clean_lme4 <- function(Response, Geno , Num_desv=3, Show_results=TRUE, data=NULL
     
     if ( sum( abs(res) > 3 ) ){
       
-      ix = c(ix, names( which( abs(res)>3 ) ))  # which.max( abs(res) )
+      ix = c(ix, names( which( abs(res)>3 ) )) %>%  as.numeric() # which.max( abs(res) )
       message("There are ", sum(abs(res) > 3), " NA values remaining")
       
       remTMP <- data.frame(Response=Datos[ix,Response],  Genotype = Datos[ix,Geno], col=Datos[ix,rep],row=Datos[ix,block] )
       remFinal <- rbind(remTMP,remFinal)
       
-      Datos[ix,"YDHA"] = NA
+      Datos[ix,Response] = NA
       
     } else {
       
@@ -63,7 +64,7 @@ Clean_lme4 <- function(Response, Geno , Num_desv=3, Show_results=TRUE, data=NULL
   }
   
   d <- broom.mixed::augment(ranef(Mo))
-  d <- d[d$grp==Genotype,c("level","estimate","std.error")]
+  d <- d[d$grp==Geno,c("level","estimate","std.error")]
   d <- data.frame(level=d[,1],estimate=round(d[,2],2),std.error=round(d[,3],2))
   BLUPs<-d
   
@@ -72,7 +73,7 @@ Clean_lme4 <- function(Response, Geno , Num_desv=3, Show_results=TRUE, data=NULL
   
   
   Modelo.vars   <- as.data.table(VarCorr(Mo))
-  VarG <- as.numeric(Modelo.vars[grp==Genotype,'vcov'])
+  VarG <- as.numeric(Modelo.vars[grp==Geno,'vcov'])
   VarE <- as.numeric(Modelo.vars[grp=='Residual','vcov'])
   replicate <- length(unique(Datos[,rep]))
   r2 <- as.numeric(r.lme4(Mo))
